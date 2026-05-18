35146aabf8ffb05ba4f4803f5be0ac3082dbc2a6
Joel Thorstensson <joel@stigmergic.org>
2026-05-18 18:12:45 +0100

    fix: linux build

--- src/tray.rs
+++ src/tray.rs
@@ -8,7 +8,7 @@ use std::time::{Duration, Instant};
 
 #[cfg(target_os = "linux")]
 use directories::BaseDirs;
-use eyre::{Result, WrapErr};
+use eyre::{ContextCompat, Result, WrapErr};
 use helios::ethereum::EthereumClient;
 use image::GenericImageView;
 #[cfg(target_os = "linux")]
